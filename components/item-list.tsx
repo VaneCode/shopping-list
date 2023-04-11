6bd18e4a1ce654bdc55c66b5a3a7a9e8ec499ce5
vanessaBonito <vanebonito@hotmail.com>
2023-04-11 11:06:25 -0500

    Create basic structure for component Item

--- components/item-list.tsx
+++
@@ -1,4 +0,0 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from 'react-native';
-import BouncyCheckbox from "react-native-bouncy-checkbox";
-import IItem from "../models/item.model";
---
+++ components/item.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import IItem from "../models/item.model";
+
+interface IItemProps {
+    data: IItem
+}
+
+const Item = (props: IItemProps) => {
+    const [bought, setBought] = useState(false);
+
+    return (
+        <View>
+            <BouncyCheckbox />
+            <Text></Text>
+        </View>
+    )
+}
+
+export default Item;
