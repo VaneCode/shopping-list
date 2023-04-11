1c22bd42622ad2bc5cf0250b7ad40bf5e8f14470
vanessaBonito <vanebonito@hotmail.com>
2023-04-11 09:47:07 -0500

    Import libraries in component item

---
+++ components/item-list.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+import IItem from "../models/item.model";
