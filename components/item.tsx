98c71877ac7e5dfa3018db58323daa84ca7c93ef
vanessaBonito <vanebonito@hotmail.com>
2023-06-02 10:41:28 -0500

    Add component Item

--- components/item.tsx
+++ components/item.tsx
@@ -1,21 +1,56 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import IItem from "../models/item.model";
 
 interface IItemProps {
-    data: IItem
+  data: IItem;
 }
 
 const Item = (props: IItemProps) => {
-    const [bought, setBought] = useState(false);
+  const [isBought, setBought] = useState(false);
 
-    return (
-        <View>
-            <BouncyCheckbox isChecked={bought} onPress={setBought}/>
-            <Text>{props.data.text}</Text>
-        </View>
-    )
-}
+  return (
+    <View style={[styles.container]}>
+      <BouncyCheckbox
+        fillColor="black"
+        unfillColor="#FFFFFF"
+        iconStyle={{ borderColor: "black" }}
+        isChecked={isBought}
+        onPress={setBought}
+        style={styles.checkbox}
+      />
+
+      <Text style={styles.text}>{props.data.text}</Text>
+    </View>
+  );
+};
 
 export default Item;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    minHeight: "3rem",
+    height: "auto",
+    color: "white",
+    backgroundColor: "#ff6666",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    display: "flex",
+    flexDirection: "row",
+    //marginBottom: 5,
+    //padding: 10,
+    gap: 5,
+    borderRadius: 5,
+  },
+  checkbox: {
+    width: 40,
+    minWidth: 40,
+    height: 40,
+  },
+  text: {
+    color: "black",
+    width: "100%",
+  },
+});
